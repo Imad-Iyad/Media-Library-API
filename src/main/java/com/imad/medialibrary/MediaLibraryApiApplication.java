509ef34a7871d8f5d55f9587034c107134c701bd
Imad Iyad <emadeyad063@gmail.com>
2025-07-15 12:15:50 +0300

    first commit

---
+++ src/main/java/com/imad/medialibrary/MediaLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.imad.medialibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaLibraryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaLibraryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imad/medialibrary/MediaLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imad.medialibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaLibraryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
